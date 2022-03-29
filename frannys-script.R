3ea36e440751aacd184ace854da8079b292e52a7
Chris Merkord <chrismerkord@gmail.com>
2022-03-28 22:21:14 -0500

    Add franny script

---
+++ frannys-script.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+
+# MDE data ----------------------------------------------------------------
+
+# 2020
+load("data/NSDUH_2020.Rdata")               # read 2020 data
+nsduh_2020 <- as_tibble(NSDUH_2020)         # turn it into a tibble
+amdeyr_2020 <-                              # get MDE as a percent
+  nsduh_2020 %>% 
+  filter(AGE2 >= 7, !is.na(amdeyr)) %>% 
+  select(amdeyr) %>% 
+  count(amdeyr) %>% 
+  mutate(
+    amdeyr_per = amdeyr / sum(amdeyr),
+    year = 2020
+  ) %>% 
+  print()
+
+# 2019
+load("data/NSDUH_2019.Rdata")               # read 2019 data
+nsduh_2019 <- as_tibble(NSDUH_2020)         # turn it into a tibble
+amdeyr_2019 <-                              # get MDE as a percent
+  nsduh_2020 %>% 
+  filter(AGE2 >= 7, !is.na(amdeyr)) %>% 
+  select(amdeyr) %>% 
+  count(amdeyr) %>% 
+  mutate(
+    amdeyr_per = amdeyr / sum(amdeyr),
+    year = 2019
+  ) %>% 
+  print()
+
+bind_rows(
+  amdeyr_2020,
+  amdeyr_2019
+)
