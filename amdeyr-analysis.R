dd5d8118153f5cbce6f77dab506b10fba360af05
Franny <francesca.r.stewart@gmail.com>
2022-03-31 18:54:45 -0500

    commit

--- amdeyr-analysis.R
+++ amdeyr-analysis.R
@@ -1,10 +1,17 @@
 
 library(tidyverse)
 
+amdeyr_2020 <- read_csv("data/amdeyr_2020.csv")
 amdeyr_2019 <- read_csv("data/amdeyr_2019.csv")
+amdeyr_2018 <- read_csv("data/amdeyr_2018.csv")
+amdeyr_2017 <- read_csv("data/amdeyr_2017.csv")
+amdeyr_2016 <- read_csv("data/amdeyr_2016.csv")
 
 bind_rows(
   amdeyr_2020,
   amdeyr_2019,
-  amdeyr_2018
+  amdeyr_2018,
+  amdeyr_2017,
+  amdeyr_206)
+  
 )
--- process-amdeyr.R
+++ process-amdeyr.R
@@ -16,6 +16,11 @@ amdeyr_2020 <-                              # get MDE as a percent
   ) %>% 
   print()
 
+write_csv(
+  x = amdeyr_2020,
+  file = "data/amdeyr_2020.csv",
+  col_names = TRUE
+)
 
 # 2019
 load("data/NSDUH_2019.RData")               # read 2019 data
@@ -38,4 +43,66 @@ write_csv(
   col_names = TRUE
 )
 
+#2018
+load("data/NSDUH_2018.RData")               # read 2018 data
+nsduh_2018 <- as_tibble(PUF2018_100819)         # turn it into a tibble
+nsduh_2018
+amdeyr_2018 <-                              # get MDE as a percent
+  nsduh_2018 %>% 
+  filter(AGE2 >= 7, !is.na(amdeyr)) %>% 
+  select(amdeyr) %>% 
+  count(amdeyr) %>% 
+  mutate(
+    p = n / sum(n),
+    year = 2018
+  ) %>% 
+  print()
+
+write_csv(
+  x = amdeyr_2018,
+  file = "data/amdeyr_2018.csv",
+  col_names = TRUE
+)
+
+#2017
+load("data/NSDUH_2017.RData")               # read 2017 data
+nsduh_2017 <- as_tibble(PUF2017_100918)         # turn it into a tibble
+nsduh_2017
+amdeyr_2017 <-                              # get MDE as a percent
+  nsduh_2017 %>% 
+  filter(AGE2 >= 7, !is.na(amdeyr)) %>% 
+  select(amdeyr) %>% 
+  count(amdeyr) %>% 
+  mutate(
+    p = n / sum(n),
+    year = 2017
+  ) %>% 
+  print()
+
+write_csv(
+  x = amdeyr_2017,
+  file = "data/amdeyr_2017.csv",
+  col_names = TRUE
+)
+
+#2016
+load("data/NSDUH_2016.RData")               # read 2016 data
+nsduh_2016 <- as_tibble(NSDUH_2016)         # turn it into a tibble
+nsduh_2016
+amdeyr_2016 <-                              # get MDE as a percent
+  nsduh_2016 %>% 
+  filter(AGE2 >= 7, !is.na(amdeyr)) %>% 
+  select(amdeyr) %>% 
+  count(amdeyr) %>% 
+  mutate(
+    p = n / sum(n),
+    year = 2016
+  ) %>% 
+  print()
+
+write_csv(
+  x = amdeyr_2016,
+  file = "data/amdeyr_2016.csv",
+  col_names = TRUE
+)
 
